e7530df4903a5a6a5f1b821f15aea9479477f983
abduarrahman <abdu.arrahman@bukitmakmur.com>
2023-02-13 23:17:19 +0700

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Muhammad Abdu Ar Rahman on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
